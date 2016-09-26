e7dbcd27a2585e31a25b2bd609006ca0283fb737
¨KasperHdL¨ <kasper@honnens.dk>
2016-09-26 14:23:33 +0200

    Super Simple Gravity

---
+++ Assets/Gravity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+	public Rigidbody2D[] bodies;
+	public float gravityCoef;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0;i < bodies.Length;i++){
+			bodies[i].AddForce(gravityCoef * (transform.position - bodies[i].transform.position));
+		}
+	}
+}
