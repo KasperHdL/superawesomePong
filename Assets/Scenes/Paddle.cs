c8f4a9635c201d46df31cd0061a34d99c15eec5b
Morten Drescher Salling <m.drescher.s@gmail.com>
2016-09-26 15:32:10 +0200

    setable keycodes

--- Assets/Scenes/Paddle.cs
+++ Assets/Scenes/Paddle.cs
@@ -5,6 +5,10 @@ public class Paddle : MonoBehaviour {
 
     float angle;
     float speed = 2;
+
+    public KeyCode right { get; set; }
+    public KeyCode left { get; set; }
+
 	// Use this for initialization
 	void Start () {
         angle = Mathf.Asin(transform.position.y / 2.66f);
@@ -12,17 +16,17 @@ public class Paddle : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKey(KeyCode.A))
+	    if(Input.GetKey(right))
         {
             angle -= Time.deltaTime * 2;        
         }
-        else if(Input.GetKey(KeyCode.D))
+        else if(Input.GetKey(left))
         {
             angle += Time.deltaTime * 2;
         }
         var x = Mathf.Cos(angle) * 2.66f;
         var y = Mathf.Sin(angle) * 2.66f;
         transform.position = new Vector3(x, y, 0);
-        transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg + 90);
+        transform.rotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
 	}
 }
