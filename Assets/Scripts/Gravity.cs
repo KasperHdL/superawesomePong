8e711cf94ebb5d2c840b7caa06fff2ec1296af21
¨KasperHdL¨ <kasper@honnens.dk>
2016-09-27 20:28:04 +0200

    Made multiple versions of gravity

--- Assets/Scripts/Gravity.cs
+++ Assets/Scripts/Gravity.cs
@@ -3,16 +3,38 @@ using System.Collections;
 
 public class Gravity : MonoBehaviour {
 	public Rigidbody2D[] bodies;
-	public float gravityCoef;
+	public float gravityCoef_oneOverDistSqr;
+	public float gravityCoef_distSqr;
+	public float gravityCoef_dist;
+
+	public CalcMethod calculationMethod;
+
+	public enum CalcMethod{
+		oneOverDistanceSquared,
+		distanceSquared,
+		distance,
+	}
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		for(int i = 0;i < bodies.Length;i++){
-			bodies[i].AddForce(gravityCoef * (transform.position - bodies[i].transform.position));
+			Vector3 delta =(transform.position - bodies[i].transform.position);
+			switch(calculationMethod){
+				case CalcMethod.oneOverDistanceSquared:
+					bodies[i].AddForce((gravityCoef_oneOverDistSqr / Vector3.Dot(delta,delta)) * delta.normalized);
+				break;
+				case CalcMethod.distanceSquared:
+					bodies[i].AddForce((gravityCoef_distSqr * Vector3.Dot(delta, delta)) * delta.normalized);
+				break;
+				case CalcMethod.distance:
+					bodies[i].AddForce(gravityCoef_dist * delta);
+
+				break;
+			}
 		}
 	}
 }
