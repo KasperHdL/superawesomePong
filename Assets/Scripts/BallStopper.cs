0f3b10c51bcaad2c04e1cee14c6f9ee95a09b0ba
¨KasperHdL¨ <kasper@honnens.dk>
2016-09-27 20:27:53 +0200

    Added Edges to the playingfield that zeroes ball velocity

---
+++ Assets/Scripts/BallStopper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallStopper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.gameObject.tag == "Ball"){
+			Rigidbody2D body = coll.gameObject.GetComponent<Rigidbody2D>();
+			body.velocity = Vector2.zero;
+		}
+
+	}
+}
